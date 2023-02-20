b8a393971afd4855459d508f80d149c3a423a46d
Volgin7 <8612020@gmail.com>
2023-02-20 13:07:56 +0300

    +task2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
@@ -13,6 +14,19 @@ public class Main {
             System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
         } else {
             System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать ");
+        }
+
+    }
+    public static void task2() {
+
+        System.out.println("Задача 2");
+
+        float temperature = 4.9f;
+
+        if (temperature < 5.0f) {
+            System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+        } else {
+            System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
         }
 
     }
