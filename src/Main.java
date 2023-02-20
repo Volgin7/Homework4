f3da5a8289447cf5bd3e97aa940b3ad06f29c700
Volgin7 <8612020@gmail.com>
2023-02-20 12:57:59 +0300

    +task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+
+        System.out.println("Задача 1");
+
+        int age = 18;
+
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
+        } else {
+            System.out.println("Если возраст человека равен " + age + ", то он не достиг совершеннолетия, нужно немного подождать ");
+        }
+
     }
 }
