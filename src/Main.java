b8cd4d031e62a84c3cbfd70195c0ab458519869e
Volgin7 <8612020@gmail.com>
2023-02-21 06:44:22 +0300

    +task4

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,12 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1() {
-
         System.out.println("Задача 1");
-
         int age = 18;
-
         if (age >= 18) {
             System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
         } else {
@@ -18,11 +16,8 @@ public class Main {
         }
     }
     public static void task2() {
-
         System.out.println("Задача 2");
-
         float temperature = 4.9f;
-
         if (temperature < 5.0f) {
             System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
         } else {
@@ -30,15 +25,34 @@ public class Main {
         }
     }
     public static void task3() {
-
         System.out.println("Задача 3");
-
         float velocity = 60.1f;
-
         if (velocity > 60.0f) {
             System.out.println("Если скорость " + velocity + " то придется заплатить штраф");
         } else {
             System.out.println("Если скорость " + velocity + " то можно ездить спокойно");
         }
     }
+    public static void task4() {
+        System.out.println("Задача 4");
+        int age = 25;
+        if (age >= 2 && age <= 6) {
+            System.out.println("Если возраст человека равен " + age + ", то ему надо ходить в детский сад");
+        } else {
+            if (age >= 7 && age <= 18) {
+                System.out.println("Если возраст человека равен " + age + ", то ему надо ходить в школу");
+            } else {
+                if (age > 18 && age < 24) {
+                    System.out.println("Если возраст человека равен " + age + ", то ему надо ходить в университет");
+                } else {
+                    if (age > 24) {
+                        System.out.println("Если возраст человека равен " + age + ", то ему надо ходить на работу");
+                    }
+                }
+            }
+
+        }
+
+    }
+
 }
